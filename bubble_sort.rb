93e3055071222d15155efc3869e384c9d244c527
Juan <jacobo12.montes@gmail.com>
2020-07-10 11:44:31 -0500

    fix the linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,17 @@
 def bubble_sort(array)
-    swap=1
-    sorted_element=0
-    while swap!=0
-        swap=0
-        for j in 0...array.length-sorted_element-1 do
-            if array[j]>array[j+1]
-                array[j],array[j+1]=array[j+1],array[j]
-                swap+=1
-            end
-        end
-        sorted_element+=1
+  swap = 1
+  sorted_element = 0
+  while swap != 0
+    swap = 0
+    for j in 0...array.length - sorted_element - 1 do
+      if array[j] > array[j + 1]
+        array[j], array[j + 1] = array[j + 1], array[j]
+        swap += 1
+      end
     end
-    array
+    sorted_element += 1
+  end
+  array
 end
-a=[4,3,78,2,0,2]
-p bubble_sort(a)
+a = [4, 3, 78, 2, 0, 2]
+bubble_sort(a)
