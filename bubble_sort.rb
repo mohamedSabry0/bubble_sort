20ac89ded749662f629a464de45f02ecaed710fc
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-07-10 22:01:01 +0200

    fixing linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,15 @@
+require 'pry'
 def bubble_sort(array)
   swap = 1
   sorted_element = 0
   while swap != 0
     swap = 0
-    for j in 0...array.length - sorted_element - 1 do
-      if array[j] > array[j + 1]
-        array[j], array[j + 1] = array[j + 1], array[j]
+    array.each_with_index do |_item, index|
+      break if index == array.length - 1 - sorted_element
+
+      # binding.pry
+      if array[index] > array[index + 1]
+        array[index], array[index + 1] = array[index + 1], array[index]
         swap += 1
       end
     end
@@ -14,4 +18,4 @@ def bubble_sort(array)
   array
 end
 a = [4, 3, 78, 2, 0, 2]
-bubble_sort(a)
+p bubble_sort(a)
